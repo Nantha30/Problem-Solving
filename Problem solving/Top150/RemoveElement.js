7d6485d2e0c6c8e04f7740a3cd08bc174f4c6a6f
Nantha30 <125810582+Nantha30@users.noreply.github.com>
2023-09-23 21:59:37 +0530

    Add files via upload

---
+++ Problem solving/Top150/RemoveElement.js
@@ -0,0 +1,13 @@
+var removeElement = function (nums, val) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+
+  console.log(i);
+};
+
+removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2);
